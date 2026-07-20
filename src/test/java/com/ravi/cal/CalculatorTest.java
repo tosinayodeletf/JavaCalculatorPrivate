b1aa3ef8bd31636b8c11105ca3c7695225440087
EC2 Default User <ec2-user@ip-172-31-39-23.ec2.internal>
2026-07-20 23:24:08 +0000

    Add CalculatorTest JUnit test

---
+++ src/test/java/com/ravi/cal/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.ravi.cal;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+        Calculator calc = new Calculator();
+        assertEquals(5, calc.add(2, 3));
+    }
+}
